f876829fea59901c9d3de5eaf250d8614761014c
vamsi makaraju <vamsi.makaraju@achiever-40.local>
2019-07-27 10:59:59 +0530

    Created one repository file

--- CreatingRepository/ViewController.swift
+++ CreatingRepository/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Bro.")
+        print("How are you.")
+        print("Have a good day.")
+        print("Crash 1.")
+        print("Crash 2.")
+        print("Crash 3.")
+        print("Crash 4.")
     }
 
 
